71e3bdef44da656d8db08b255734b786562c83d8
Cesar Bernal <mrcode.hack28@gmail.com>
2023-08-12 23:41:58 -0700

    Fixing code

--- Blink.ino
+++ Blink.ino
@@ -1,32 +1,14 @@
 #include "wifi_connection.h"
 #include "websocket_connection.h"
 
-WiFiConnection wifiConnection;
-
 const int motorPin1 = 22;
 const int motorPin2 = 1; 
 const int motorPin3 = 3; 
 const int motorPin4 = 21;
+const int stepSequence[] = { B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001 };
+const int speed = 10;
 
-const int pasoSecuencia[] = { B1000, B1100, B0100, B0110, B0010, B0011, B0001, B1001 };
-
-const int velocidadMotor = 10;
-
-void avanzarUnPaso() {
-   unsigned long startTime = millis();
-
-  while (millis() - startTime < 5000) {
-    for (int i = 0; i < 8; i++) {
-      int paso = pasoSecuencia[i];
-      digitalWrite(motorPin1, bitRead(paso, 0));
-      digitalWrite(motorPin2, bitRead(paso, 1));
-      digitalWrite(motorPin3, bitRead(paso, 2));
-      digitalWrite(motorPin4, bitRead(paso, 3));
-      delay(velocidadMotor);
-    }
-  }
-  webSocketConnection.setMotorState(false);
-}
+WiFiConnection wifiConnection;
 
 void setup() {
   Serial.begin(9600);
@@ -37,11 +19,7 @@ void setup() {
     Serial.println("Connected to WiFi");
     
     webSocketConnection.begin();
-
-    pinMode(motorPin1, OUTPUT);
-    pinMode(motorPin2, OUTPUT);
-    pinMode(motorPin3, OUTPUT);
-    pinMode(motorPin4, OUTPUT);
+    motorBegin();
   }
   else {
     Serial.println("Failed to connect to WiFi");
@@ -50,8 +28,32 @@ void setup() {
 
 void loop() {
   webSocketConnection.loop();
-  
+
   if(webSocketConnection.getMotorState() == true) {
-    avanzarUnPaso();
+    spinMotor();
   }
+}
+
+void motorBegin() {
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+}
+
+void spinMotor() {
+  unsigned long startTime = millis();
+
+  while (millis() - startTime < 5000) {
+    for (int i = 0; i < 8; i++) {
+      int step = stepSequence[i];
+      digitalWrite(motorPin1, bitRead(step, 0));
+      digitalWrite(motorPin2, bitRead(step, 1));
+      digitalWrite(motorPin3, bitRead(step, 2));
+      digitalWrite(motorPin4, bitRead(step, 3));
+      delay(speed);
+    }
+  }
+
+  webSocketConnection.setMotorState(false);
 }
